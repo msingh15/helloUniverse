b20afc2874ae014a347b8ca85191502b36d230c4
msingh15 <122344676+msingh15@users.noreply.github.com>
2023-01-12 17:01:02 -0800

    Initial Commit

---
+++ sourcecodedemo/sourcecodedemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  sourcecodedemoApp.swift
+//  sourcecodedemo
+//
+//  Created by user227646 on 1/12/23.
+//
+
+import SwiftUI
+
+@main
+struct sourcecodedemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
